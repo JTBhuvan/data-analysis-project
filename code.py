4ead567cb7153c68694043c43a1dfc0ecd0cb17d
JTBhuvan <59383522+JTBhuvan@users.noreply.github.com>
2020-07-29 19:00:11 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import csv
+import plotly.graph_objects as go
+import plotly.express as px
+
+df=pd.read_csv("data.csv")
+#print(df.groupby("level")["attempt"].mean())
+fig=px.scatter(
+         x=df.groupby("level")["attempt"].mean(),
+         y=['Level 1','Level 2','Level 3','Level 4'],
+         
+)
+fig.show()
+
+
+
+
+
+
